4c501b335af5212e4a0f8c1b5ecce9f11ba25af3
YURII SILIN <try2scumme@gmail.com>
2023-04-24 20:41:06 +0300

    1

--- api/index.js
+++ api/index.js
@@ -9,3 +9,5 @@ app.get('/test', (req, res) => {
 app.post('/register', (req, res) => { })
 
 app.listen(4000)
+
+//mongodb+srv://admin:l5wVsKHsbcZBI9tX@patriot.ghzx2ys.mongodb.net/?retryWrites=true&w=majority
